cf9a82e05e51effbc5f435e5ec011b6bb9d1acbd
Wilfredo Rujel <66637170+wrujel@users.noreply.github.com>
2024-01-13 10:21:18 -0500

    add transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Cover from "@/components/cover/Cover";
+import Transition from "@/components/transition/transition";
 
 export default function Home() {
   return (
     <main className="h-[100vh]">
+      <Transition />
       <Cover />
     </main>
   );
